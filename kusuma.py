1512071c43c3f2ff09bbf6c9be6763e5501507ed
Kusuma <kusuma192006@gmail.com>
2026-07-08 23:28:42 +0530

    Added Python practice programs

---
+++ kusuma.py
@@ -0,0 +1,15 @@
+'''Create a Salary Calculator.
+Store:
+Basic Salary
+Bonus
+Tax
+Calculate:
+Gross Salary
+Net Salary'''
+basic_salary=50000
+bonus=40000
+tax=3000
+gross_salary=basic_salary+bonus
+net_salary=gross_salary-tax
+print("gross_salary : ", gross_salary)
+print("Net_salary : ",net_salary)
