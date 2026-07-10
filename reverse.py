4575eda1262e72492bbc06f4133f397f96ffd1a9
Kusuma <kusuma192006@gmail.com>
2026-07-10 17:10:07 +0530

    Added Python string practice programs

--- reverse.py
+++
@@ -1,7 +0,0 @@
-#reverse every word
-a="I Love Python"
-words=a.split()
-for word in words:
-    print(word[::-1],end=" ")
-
-    
---
+++ strings.py
@@ -0,0 +1,9 @@
+#check whether two strings are equal
+a="Kusuma"
+b="Amulya"
+if a==b:
+    print("string are equal")
+else:
+    print("strings are not equal")
+
+    
