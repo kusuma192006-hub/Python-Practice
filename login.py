79f7864d65d1a58849f337d9626500a5a1da658a
Kusuma <kusuma192006@gmail.com>
2026-07-15 20:37:40 +0530

    Add program to find second largest and second smallest numbers

--- login.py
+++ login.py
@@ -1,9 +1,1 @@
-#Login_system
-username="admin"
-password="python123"
-username=input("enter username : ")
-password=input("enter password : ")
-if username=="admin" and password=="python123":
-    print("Login Successful")
-else:
-    print("Invalid Credentials")
+#flight ticket booking
