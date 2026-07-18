4ddc6e40953f9f907f03ad88d15cee73745d1654
Kusuma <kusuma192006@gmail.com>
2026-07-18 21:02:31 +0530

    Add program to find common elements using sets

--- marks.py
+++ marks.py
@@ -1,30 +1,9 @@
-#Student Marks Analyzer
-marks=(100,34,89,35,78,36,56,45,54,90)
-student_above_35=0
-student_below_35=0
-Highest_mark=max(marks)
-Lowest_mark=min(marks)
-total=sum(marks)
-average=total/len(marks)
-for mark in marks:
-    if mark>=35:
-        student_above_35+=1
-    else:
-        student_below_35+=1
-print("Student Mars Analysis : ")
-print("The Highest mark is : ",Highest_mark)
-print("The Student Lowest mark is : " , Lowest_mark)
-print("The Average is : " , average)
-print("The Number of students got above 35 : " , student_above_35)
-print("The Number of students failed : " ,student_below_35)
-print("\n Grades : ")
-for mark in marks:
-    if mark>=90:
-        print(mark, " : Grade A")
-    elif mark>75:
-        print(mark, ": Grade B")
-    elif mark>=35:
-        print(mark , " : Grade C")
-    else:
-        print(mark, " : Fail ")
+s={1,2,3,4,5,6,7,8,9,10}
+copy_set=s.copy()
+print(" The original set is : " , s)
+print("The copied set is : ", copy_set)
+copy_set.add(11)
+print(" \n After adding 11 to the copied set")
+print(" Original set : ",s)
+print(" The copied set is : " , copy_set)
 
---
+++ s.py
@@ -0,0 +1,6 @@
+#common elements between two lists using set
+l1=[1,2,3,4,5,6]
+l2=[4,5,6,7,8,9]
+s1=set(l1)
+s2=set(l2)
+print(s1 &s2)
