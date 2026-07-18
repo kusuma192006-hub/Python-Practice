1b3e3566a1f33ec9fbd985ad64db8d4fe02fc811
Kusuma <kusuma192006@gmail.com>
2026-07-18 22:10:21 +0530

    Add program to find common subjects using set intersection

---
+++ common.py
@@ -0,0 +1,5 @@
+#Find the common subjects between two students
+student1={"Math","Python","Physics"}
+student2={"Python","Math","Chemistry"}
+common=student1 & student2
+print("The Common subjects between two students are : " , common)
