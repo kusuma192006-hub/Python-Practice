cdcc404e87eaf07afd10edb279e5a948b5a06d1b
Kusuma <kusuma192006@gmail.com>
2026-07-21 15:05:11 +0530

    Sort dictionary keys in Python

---
+++ keys.py
@@ -0,0 +1,9 @@
+#Sort dictionary by keys
+d={
+   "name":"kusuma",
+   "hobby":"watching movies",
+   "college":"nitte",
+   "branch":"aero"
+}
+sorting=sorted(d.keys())
+print("The sorted keys are : " ,sorting )
