5afb113cff6ad35b7e67903e65dc50c0ab597e7b
Kusuma <kusuma192006@gmail.com>
2026-07-21 14:49:04 +0530

    Merge two dictionaries using update() method

---
+++ 4.py
@@ -0,0 +1,13 @@
+#Merge two dictionaries
+d1={
+    "name":"kusuma",
+    "age":22,
+    "hobby":"watchning movies"
+}
+d2={
+    "name":"mallika",
+    "age":20,
+    "hobby":"drawing"
+}
+d1.update(d2)
+print(d1)
